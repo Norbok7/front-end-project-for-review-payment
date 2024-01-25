b227dc3e2deb2f013096c9d89912c2895e4134a1
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-01-25 11:01:26 -0600

    header done working on nav

--- angular-project-demo/src/app/app.component.ts
+++ angular-project-demo/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PerformanceplusComponent } from './performanceplus/performanceplus.component';
-
+import { HeaderComponent } from './header/header.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PerformanceplusComponent],
+  imports: [CommonModule, RouterOutlet, PerformanceplusComponent, HeaderComponent, NavigationBarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-project-demo/src/app/app.routes.ts
+++ angular-project-demo/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RouterModule, } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { EmployeesComponent } from './performanceplus/employees/employees.component';
+import { EmployeePayComponent } from './performanceplus/employee-pay/employee-pay.component';
+import { CostComponent } from './cost/cost.component';
+import { ProgramsComponent } from './programs/programs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'programs', component: ProgramsComponent },
+  { path: 'cost', component: CostComponent },
+];
